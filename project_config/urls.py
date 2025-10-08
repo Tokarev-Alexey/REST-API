f900020de412a7bf8f97bff8450a8c74ec1e2c94
Tokarev-Alexey <leha_motaro@mail.ru>
2025-10-08 18:05:05 +0700

    Добавил доккументацию swagger

--- project_config/urls.py
+++ project_config/urls.py
@@ -17,10 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import re_path
+
+from project_config.swagger import schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('rest_framework.urls')),
     path('api-token-auth/', views.obtain_auth_token),
     path('', include('api.urls')),  # в API urls лежат endpoints к моделям проекта
-]
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
